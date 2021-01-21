5059b49e3d211d3e9f3936f6ca313b665a042cd0
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2021-01-21 14:19:07 +0100

    cocktail bars

--- src/js/index.js
+++ src/js/index.js
@@ -7,15 +7,41 @@ import "../scss/main.scss";
 /* place your code below */
 
 window.addEventListener("load", (event) => {
-    console.log("page is fully loaded");
-    document.body.classList.remove("body--preload");
-  });
+  console.log("page is fully loaded");
+  document.body.classList.remove("body--preload");
+});
 
 const buttonMenu = document.querySelector(".navigation__menu");
 const menuList = document.querySelector(".navigation__list--js");
+
+const gooseButton = document.querySelector(".section__button--goose");
+const sababaButton = document.querySelector(".section__button--sababa");
+const mercyButton = document.querySelector(".section__button--mercy");
 
 buttonMenu.addEventListener("click", () => {
   menuList.classList.toggle("navigation__list--open");
   buttonMenu.classList.toggle("navigation__menu");
   buttonMenu.classList.toggle("navigation__menu--open");
+});
+
+gooseButton.addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--translate-goose",
+    "translateY(0%)"
+  );
+  document.documentElement.style.setProperty("--opacity-goose", "1");
+});
+sababaButton.addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--translate-sababa",
+    "translateY(0%)"
+  );
+  document.documentElement.style.setProperty("--opacity-sababa", "1");
+});
+mercyButton.addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--translate-mercy",
+    "translateY(0%)"
+  );
+  document.documentElement.style.setProperty("--opacity-mercy", "1");
 });
