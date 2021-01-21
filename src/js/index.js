b678c86061f0834672cd745f2dd3da025dba3804
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2021-01-21 21:05:46 +0100

    bars section changes

--- src/js/index.js
+++ src/js/index.js
@@ -15,8 +15,14 @@ const buttonMenu = document.querySelector(".navigation__menu");
 const menuList = document.querySelector(".navigation__list--js");
 
 const gooseButton = document.querySelector(".section__button--goose");
+const gooseTile = document.querySelector(".section__tile--goose");
+const gooseLink = document.querySelector(".section__link--goose");
 const sababaButton = document.querySelector(".section__button--sababa");
+const sababaTile = document.querySelector(".section__tile--sababa");
+const sababaLink = document.querySelector(".section__link--sababa");
 const mercyButton = document.querySelector(".section__button--mercy");
+const mercyTile = document.querySelector(".section__tile--mercy");
+const mercyLink = document.querySelector(".section__link--mercy");
 
 buttonMenu.addEventListener("click", () => {
   menuList.classList.toggle("navigation__list--open");
@@ -25,23 +31,17 @@ buttonMenu.addEventListener("click", () => {
 });
 
 gooseButton.addEventListener("click", () => {
-  document.documentElement.style.setProperty(
-    "--translate-goose",
-    "translateY(0%)"
-  );
-  document.documentElement.style.setProperty("--opacity-goose", "1");
+  gooseButton.classList.toggle("section__button--goose-rotated");
+  gooseTile.classList.toggle("section__tile--goose-open");
+  gooseLink.classList.toggle("section__link--goose-open");
 });
 sababaButton.addEventListener("click", () => {
-  document.documentElement.style.setProperty(
-    "--translate-sababa",
-    "translateY(0%)"
-  );
-  document.documentElement.style.setProperty("--opacity-sababa", "1");
+  sababaButton.classList.toggle("section__button--sababa-rotated");
+  sababaTile.classList.toggle("section__tile--sababa-open");
+  sababaLink.classList.toggle("section__link--sababa-open");
 });
 mercyButton.addEventListener("click", () => {
-  document.documentElement.style.setProperty(
-    "--translate-mercy",
-    "translateY(0%)"
-  );
-  document.documentElement.style.setProperty("--opacity-mercy", "1");
+  mercyButton.classList.toggle("section__button--mercy-rotated");
+  mercyTile.classList.toggle("section__tile--mercy-open");
+  mercyLink.classList.toggle("section__link--mercy-open");
 });
